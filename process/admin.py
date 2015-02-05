378b2a1840f5e070e42deffa1a7e7e8346b76464
bebr42 <brincken@orgraum.de>
2015-02-05 03:37:28 +0100

    admin.py > Alle Tabellen aus Models registriert

--- process/admin.py
+++ process/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from process.models import ProcessStep, ProcessDef, StatusScheme, FieldPerstep,  FieldDef, RoleDef, ProcInstance, RoleInstance, PycLog
+
+
+admin.site.register(ProcessStep)
+admin.site.register(ProcessDef)
+admin.site.register(StatusScheme)
+admin.site.register(FieldPerstep)
+admin.site.register(FieldDef)
+admin.site.register(RoleDef)
+admin.site.register(ProcInstance)
+admin.site.register(RoleInstance)
+admin.site.register(PycLog)
+
