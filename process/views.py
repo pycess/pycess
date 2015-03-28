f4357431dbc9c7e5e45990b9c000a90158e6c620
Martin Häcker <spamfaenger@gmx.de>
2015-03-28 11:31:06 +0100

    Change models import to not use the start import for ease of tracking where what comes from in the file.

--- process/views.py
+++ process/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
-from process.models import *
+from . import models
 from django.utils import timezone
 from django.views.generic import View
 from datetime import datetime
@@ -12,7 +12,7 @@ import json
 
 # REFACT: introduce pagination
 def process_index(request):
-    processes = ProcessDefinition.objects.all()
+    processes = models.ProcessDefinition.objects.all()
     instances_by_process = dict(
         (process, process.instances.filter(currentstatus__scheme_prestatus__role__role_instance__pycuser=request.user))
         for process in processes
@@ -21,7 +21,7 @@ def process_index(request):
 
 # REFACT: introduce pagination
 def process_overview(request):
-    processes = ProcessDefinition.objects.all()
+    processes = models.ProcessDefinition.objects.all()
     instances_by_process = dict(
         (process, process.instances.filter(process__steps__role__role_instance__pycuser=request.user))
         for process in processes
@@ -29,33 +29,26 @@ def process_overview(request):
     return HttpResponse(render(request, 'process/process_overview.html', locals()))
 
 def process_instance_create(request, process_id):
-    process = get_object_or_404(ProcessDefinition, pk=process_id)
-    # REFACT: use process.create_instance(creator=request.user)?
-    instance = ProcessInstance.objects.create(
-        process=process,
-        currentstep=process.first_transition(),
-        procdata=json.dumps({}), # FIXME: set initial data from somewhere
-        starttime=timezone.now(),
-        stoptime=timezone.now(),
-        status=3)
+    process = get_object_or_404(models.ProcessDefinition, pk=process_id)
+    instance = process.create_instance(request.user)
     return redirect('instance_detail', process_id=process.id, instance_id=instance.id)
 
 
 class ProcessInstanceView(View):
     
     def get(self, request, process_id, instance_id):
-        instance = get_object_or_404(ProcessInstance, pk=instance_id)
-        json_schema = instance.currentstep.json_schema
-        current_json = instance.currentstep.json_data(instance)
+        instance = get_object_or_404(models.ProcessInstance, pk=instance_id)
+        json_schema = instance.currentstatus.step.json_schema
+        current_json = instance.currentstatus.step.json_data(instance)
         return HttpResponse(render(request, 'process/process_instance_detail.html', locals()))
     
     def post(self, request, process_id, instance_id):
-        instance = get_object_or_404(ProcessInstance, pk=instance_id)
+        instance = get_object_or_404(models.ProcessInstance, pk=instance_id)
         # FIXME: validate json and then update procdata with it
         instance.procdata = request.POST['json']
         # FIXME: validate requested_transition
         if 'requested_transition_id' in request.POST:
-            status = get_object_or_404(StatusScheme, pk=request.POST['requested_transition_id'])
+            status = get_object_or_404(models.StatusScheme, pk=request.POST['requested_transition_id'])
             instance.transition_with_status(status)
             # TODO: consider adding information. Who will be responsible, what time frame, perhaps even how to follow along?
             messages.success(request, "Transitioned to status %s: %s" % (status.name, status.remark))
