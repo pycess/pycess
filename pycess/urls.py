5bdebaf70e966f6c550052874a1c57e9906ceaf4
Martin Häcker <spamfaenger@gmx.de>
2015-03-28 09:52:52 +0100

    Change to newstyle url patterns syntax to avoid warnings in django 1.8

--- pycess/urls.py
+++ pycess/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'pycess.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'pycess.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'', include('process.urls')),
-                       )
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('process.urls')),
+]
