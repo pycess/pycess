432749a041ca3678e9369445cd7649836130cbdf
Martin Häcker <spamfaenger@gmx.de>
2015-04-11 08:34:31 +0200

    Reintroduce settings and add a way to to have local settings that are not in the repo by introducing the concept of pycess/local_settings.py

---
+++ pycess/settings.py
@@ -0,0 +1,105 @@
+"""
+Django settings for pycess project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'c-e2f6t1=c%^h8pb=y=scp8(szq(q=7-18$36$oyf@*b9x)@n^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Will need to change soon as per http://jxqdjango.readthedocs.org/en/latest/ref/templates/upgrading.html
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'process',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'pycess.urls'
+
+WSGI_APPLICATION = 'pycess.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+# REFACT should this go into local_settings?
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'process/static/')
+
+# Modeled after http://stackoverflow.com/questions/1406892/elegantly-handle-site-specific-settings-configuration-in-svn-hg-git-etc
+# REFACT: I would like to replace this with a config file loader that loads a yml or ini file
+# it could default to development.ini in the root file and be overridden by something. Most likely an environment variable?
+
+# Pull in hostname-based changes.
+import socket
+HOSTNAME = socket.gethostname().lower().split('.')[0].replace('-','')
+
+try:
+    exec "from pycess.%s_settings import *" % HOSTNAME
+except ImportError:
+    pass
+
+# Pull in the local changes.
+try:
+    from myproject.local_settings import *
+except ImportError:
+    pass
--- pycess/test_settings.py
+++ pycess/test_settings.py
@@ -11,17 +11,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 # Activate development settings so we duplicate as little as possible
 from pycess.settings import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'OQJ);DfK]!F+7;7$awXHfgVnVeM}wo*5%c8.U~ToUx-xGp0M8Z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -29,16 +20,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
