bb3faf9fbc69c8559ec6722c2647b3f1d5ed438d
Martin Häcker <spamfaenger@gmx.de>
2015-07-03 12:44:02 +0200

    Switch to default unicode strings for this file.

--- process/models.py
+++ process/models.py
@@ -1,4 +1,6 @@
 # coding: utf8
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
